be39e88a05c19d2d29a9dfd55e24bae183f13827
claudioleitao <claudio.leitao@yahoo.com.br>
2021-07-10 20:15:15 -0300

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //baseUrl: 'https://lecopet.herokuapp.com/'
-  baseUrl: 'http://localhost:8080'
+  baseUrl: 'https://lecopet.herokuapp.com/'
+  //baseUrl: 'http://localhost:8080'
 };
 
 /*
